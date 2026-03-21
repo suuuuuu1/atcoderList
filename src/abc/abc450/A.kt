d2a5d0ace0938ae0fa9e5d3228a8af4771e6da20
suuuuuu1 <172761105+suuuuuu1@users.noreply.github.com>
2026-03-21 19:47:45 +0900

    gg

---
+++ src/abc/abc450/A.kt
@@ -0,0 +1,25 @@
+package abc.abc450
+
+fun main() {
+    val a = 1
+    println(a)
+}
+
+@Suppress("unused")
+private fun chmin(a: LongArray, idx: Int, v: Long): Boolean {
+    if (v < a[idx]) {
+        a[idx] = v
+        return true
+    }
+    return false
+}
+
+@Suppress("unused")
+private fun chmax(a: LongArray, idx: Int, v: Long): Boolean {
+    if (v > a[idx]) {
+        a[idx] = v
+        return true
+    }
+    return false
+}
+
---
+++ src/abc/abc450/E.kt
@@ -0,0 +1,24 @@
+package abc.abc450
+
+fun main() {
+    
+}
+
+@Suppress("unused")
+private fun chmin(a: LongArray, idx: Int, v: Long): Boolean {
+    if (v < a[idx]) {
+        a[idx] = v
+        return true
+    }
+    return false
+}
+
+@Suppress("unused")
+private fun chmax(a: LongArray, idx: Int, v: Long): Boolean {
+    if (v > a[idx]) {
+        a[idx] = v
+        return true
+    }
+    return false
+}
+
