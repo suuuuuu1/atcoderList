a26e25eb10733667c143549e5ed0faca4b14b9ff
suuuuuu1 <172761105+suuuuuu1@users.noreply.github.com>
2026-04-16 20:18:16 +0900

    gg

---
+++ src/abc/abc343/C.kt
@@ -0,0 +1,50 @@
+package abc.abc343
+
+fun main() {
+    var i = 0L
+    val n = readln().toLong()
+    var ans = 0L
+    while (i * i * i <= n) {
+        val t = i.toString()
+        if (t == t.reversed()) ans = i * i * i
+        i++
+    }
+    println(ans)
+}
+
+
+@Suppress("unused")
+private fun chmin(a: IntArray, idx: Int, v: Int): Boolean {
+    if (v < a[idx]) {
+        a[idx] = v
+        return true
+    }
+    return false
+}
+
+@Suppress("unused")
+private fun chmax(a: IntArray, idx: Int, v: Int): Boolean {
+    if (v > a[idx]) {
+        a[idx] = v
+        return true
+    }
+    return false
+}
+
+@Suppress("unused")
+private fun chmin(a: LongArray, idx: Int, v: Long): Boolean {
+    if (v < a[idx]) {
+        a[idx] = v
+        return true
+    }
+    return false
+}
+
+@Suppress("unused")
+private fun chmax(a: LongArray, idx: Int, v: Long): Boolean {
+    if (v > a[idx]) {
+        a[idx] = v
+        return true
+    }
+    return false
+}
