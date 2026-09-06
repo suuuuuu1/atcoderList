8a2e746e1a65d38f558ed6dc9f56a509c9b79e33
suuuuuu1 <172761105+suuuuuu1@users.noreply.github.com>
2026-09-06 20:00:57 +0900

    dp

--- src/betu/edpc/D.kt
+++ src/betu/edpc/D.kt
@@ -1,62 +1,44 @@
 package betu.edpc
 
+import kotlin.math.max
+
 fun main() {
-    val (n, w) = readln().trim().split(" ").map { it.toInt() }
-
-    val items = Array(n) {
-        val (wi, vi) = readln().trim().split(" ").map { it.toInt() }
-        wi to vi
-    }
-
-    // dp[weight] = その重さ以下で達成できる最大価値
-    val dp = LongArray(w + 1) { 0L }
-
+    val (n, w) = readln().split(" ").map { it.toInt() }
+    val wv = Array(n) { readln().split(" ").map { it.toInt() } }
+    val dp = Array(n + 1) { LongArray(w + 1) }
     for (i in 0 until n) {
-        val wi = items[i].first
-        val vi = items[i].second.toLong()
+        val weight = wv[i][0]
+        val value = wv[i][1].toLong()
+        for (j in 0..w) {
+            if (j >= weight) {
+                dp[i + 1][j] = max(dp[i][j], dp[i][j - weight] + value)
+            }else{
+                dp[i+1][j] = dp[i][j]
+            }
 
-        // 0/1 ナップサックなので後ろから更新
-        for (curW in w downTo wi) {
-            val cand = dp[curW - wi] + vi
-            if (cand > dp[curW]) dp[curW] = cand
+
+            System.out.printf("%6s", "i \\ j|")
+            for (j in 0..w) {
+                System.out.printf("%5d", j)
+            }
+            println()
+            println("-----|------------------------------------------------")
+
+
+            // テーブルの中身
+            for ((rowIdx, row) in dp.withIndex()) {
+                System.out.printf("%4d |", rowIdx)
+                for (v in row) {
+                    System.out.printf("%5d", v)
+                }
+                println()
+            }
+            println()
         }
+
     }
 
-    println(dp[w])
+    println(dp[n][w])
 }
 
-@Suppress("unused")
-private fun chmin(a: IntArray, idx: Int, v: Int): Boolean {
-    if (v < a[idx]) {
-        a[idx] = v
-        return true
-    }
-    return false
-}
 
-@Suppress("unused")
-private fun chmax(a: IntArray, idx: Int, v: Int): Boolean {
-    if (v > a[idx]) {
-        a[idx] = v
-        return true
-    }
-    return false
-}
-
-@Suppress("unused")
-private fun chmin(a: LongArray, idx: Int, v: Long): Boolean {
-    if (v < a[idx]) {
-        a[idx] = v
-        return true
-    }
-    return false
-}
-
-@Suppress("unused")
-private fun chmax(a: LongArray, idx: Int, v: Long): Boolean {
-    if (v > a[idx]) {
-        a[idx] = v
-        return true
-    }
-    return false
-}
